8ae19f93131769f99ef45c4159855c41fea5baba
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-08-18 13:52:41 +0200

    Initial Commit

---
+++ DateCellSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DateCellSwift
+//
+//  Created by Martinho on 18/08/15.
+//  Copyright (c) 2015 TM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
